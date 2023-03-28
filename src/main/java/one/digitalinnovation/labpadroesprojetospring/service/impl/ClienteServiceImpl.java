157937fd50ec277c69dc72eb1ba2ba9f10b0f3c5
moisesmedeiros7 <moisesmedeiros@live.com>
2023-03-28 15:45:30 -0300

    Final da aula: "Praticando com Spring: Padrões de Projeto em uma API REST"

--- src/main/java/one/digitalinnovation/labpadroesprojetospring/service/impl/ClienteServiceImpl.java
+++ src/main/java/one/digitalinnovation/labpadroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -1,8 +1,11 @@
 package one.digitalinnovation.labpadroesprojetospring.service.impl;
 
 import one.digitalinnovation.labpadroesprojetospring.model.Cliente;
+import one.digitalinnovation.labpadroesprojetospring.model.Endereco;
 import one.digitalinnovation.labpadroesprojetospring.model.ClienteRepository;
+import one.digitalinnovation.labpadroesprojetospring.model.EnderecoRepository;
 import one.digitalinnovation.labpadroesprojetospring.service.ClienteService;
+import one.digitalinnovation.labpadroesprojetospring.service.ViaCepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +13,17 @@ import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
-    // TODO Singleton: Injetar os componentes do Spring com @Autowired
-    // TODO Strategy: Implementar os métodos definidos na interface
-    // TODO Facade: Abstrair integrações com subsistemas, provendo uma interface simples
+
+    //TODO Singleton: Injetar os componentes do Spring com @Autowired
     @Autowired
     private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
+    // TODO Strategy: Implementar os métodos definidos na interface
+    // TODO Facade: Abstrair integrações com subsistemas, provendo uma interface simples
 
     @Override
     public Iterable<Cliente> buscarTodos() {
@@ -37,8 +46,32 @@ public class ClienteServiceImpl implements ClienteService {
          * FIXME Caso não exista, integrar com o ViaCep e persistir retorno.
          * FIXME Inserir Cliente, vinculando o Endereco (novo ou existente)
          */
+        //
+        salvarClienteComCep(cliente);
+        /*
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco =  enderecoRepository.findById(cep).orElseGet(() ->{ //buscando endereço no banco, caso não exista (orElseGet)...
+            Endereco novoEndereco= viaCepService.consultarCep(cep);  // busca endereço no viacep
+            enderecoRepository.save(novoEndereco);              // persiste o endereço no BD
+            return novoEndereco;
+         });
+         cliente.setEndereco(endereco); //setando o endereço no cliente
+         clienteRepository.save(cliente); //salvando cliente no BD
+         */
+    }
 
 
+    // método utilitário
+    private void salvarClienteComCep (Cliente cliente){
+        //
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco =  enderecoRepository.findById(cep).orElseGet(() ->{ //buscando endereço no banco, caso não exista (orElseGet)...
+            Endereco novoEndereco= viaCepService.consultarCep(cep);  // busca endereço no viacep
+            enderecoRepository.save(novoEndereco);              // persiste o endereço no BD
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco); //setando o endereço no cliente
+        clienteRepository.save(cliente); //salvando cliente no BD
     }
 
     @Override
@@ -49,10 +82,15 @@ public class ClienteServiceImpl implements ClienteService {
          FIXME Caso não exista, integrar com o ViaCEP e persistir retorno.
          FIXME Alterar Cliente, vinculando o Endereco (novo ou existente)
          **/
-    }
+        Optional<Cliente> clientebd = clienteRepository.findById(id);
+        if (clientebd.isPresent()){
+            salvarClienteComCep(cliente);
+        }
 
+    }
     @Override
     public void deletar(Long id) {
         //FIXME Deletar cliente por ID.
+        clienteRepository.deleteById(id);
     }
 }
